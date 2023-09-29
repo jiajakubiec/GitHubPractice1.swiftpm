1b210f8cd0d6201f0ffc1db3426865d4449bb062
Jia Jakubiec <jjakubiec4186@stu.d214.org>
2023-09-29 15:55:07 -0500

    First, I added an @State variable tasks with the array of Strings: "Mow the Lawn", "Feed the Dog", and "Grocery Shop". Then, I created a List inside the body and iterated through the array so it can display the values in the array in Text views. I also removed the VStack.

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tasks = ["Mow the lawn","Feed the Dog","Grocery Shop"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            ForEach(tasks, id:\.self) { task in
+                Text(task)
+            }
         }
     }
 }
